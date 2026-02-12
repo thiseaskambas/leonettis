c885486bc73cb79c7ea940d65607f168c0d1fd64
Thésée (Thiseas) Kambas <89133710+thiseaskambas@users.noreply.github.com>
2026-02-13 00:04:32 +0100

    fix: Set image priority to true for consistent rendering in Home component

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -52,7 +52,7 @@ export default function Home() {
                   fill
                   className="object-cover object-top"
                   sizes="100vw"
-                  priority={index === 0}
+                  priority={true}
                 />
               )}
             </div>
