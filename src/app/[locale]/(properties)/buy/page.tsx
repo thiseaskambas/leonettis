ec16a41805bd21e52be52ea1f22d697df3f29c47
Thésée (Thiseas) Kambas <89133710+thiseaskambas@users.noreply.github.com>
2026-02-12 23:39:02 +0100

    feat: assets

--- src/app/[locale]/(properties)/buy/page.tsx
+++ src/app/[locale]/(properties)/buy/page.tsx
@@ -1,11 +1,16 @@
+import { getTranslations } from 'next-intl/server';
+
 import { listingsData } from '@/app/lib/mock-data/listings-data';
 import ListingCard from '@/app/ui/ListingCard';
 
-export default function Buy() {
+export default async function Buy() {
+  const t = await getTranslations('buy');
   const listings = listingsData;
   return (
     <main className="dark:bg-tiff-gray-950 min-h-screen p-5 md:p-10">
-      <div className="grid min-w-0 grid-cols-1 gap-8 md:grid-cols-2">
+      <h1 className="text-center text-2xl font-light">{t('title')}</h1>
+      <p className="text-center font-light text-gray-600">{t('description')}</p>
+      <div className="mt-10 grid min-w-0 grid-cols-1 gap-8 md:grid-cols-2">
         {listings.map((listing, index) => (
           <ListingCard key={index} listing={listing} />
         ))}
